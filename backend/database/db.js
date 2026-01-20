eae9844dac229c748eb33242b38c6ddf70d883b4
pablojuarez07 <juarezpabloema@gmail.com>
2026-01-20 18:35:56 -0300

    conect backend to postgres in supabase

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,12 +1,8 @@
 const { Pool } = require("pg");
 
 const connection = new Pool({
-  user: process.env.DB_USER,       // ej: 'postgres'
-  host: process.env.DB_HOST,       // ej: 'localhost'
-  database: process.env.DB_NAME,   // ej: 'ventas'
-  password: process.env.DB_PASS,   // tu contraseña
-  port: process.env.DB_PORT || 5432 // normalmente 5432
-  // ssl: no lo ponemos para local
+  connectionString: process.env.DATABASE_URL, // tu URL completa de Supabase
+  ssl: { rejectUnauthorized: false }         // obligatorio para Supabase
 });
 
 module.exports = connection;
