f9e9d035272052dd11d3e8f5a4190676de5c8ce3
pablojuarez07 <juarezpabloema@gmail.com>
2026-01-22 15:40:28 -0300

    imagenes van a supabase no en backend

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,8 +1,12 @@
 const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const connection = new Pool({
-  connectionString: process.env.DATABASE_URL, // tu URL completa de Supabase
-  ssl: { rejectUnauthorized: false }         // obligatorio para Supabase
+  connectionString: process.env.DATABASE_URL,
+  ssl: isProduction
+    ? { rejectUnauthorized: false } // Supabase
+    : false                          // PostgreSQL local
 });
 
 module.exports = connection;
